0a125eb31eaad9173b676302e3ad432e5628a85d
GersonGoncalves <gerson.devpro@gmail.com>
2023-04-13 15:33:36 -0300

    feat: controller, service & repository creat book

--- src/controllers/bookControllers.js
+++ src/controllers/bookControllers.js
@@ -1,8 +1,15 @@
+import bookServices from "../services/bookServices.js";
+
 async function create(req, res) {
 
     const { name, author } = req.body;
+    const { id } = res.locals.user;
     
     try {
+
+        await bookServices.create({ name, author, userId: id });
+
+        res.sendStatus(201)
 
     } catch (err) {
         res.status(500).send(err.message)
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const bookRoutes = Router();
 
